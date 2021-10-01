08b46712a68a5fd42bd3459eaf3de0df5b742030
pentadothaxor <pentadot@protonmail.com>
2021-10-01 08:40:23 +0000

    database-test

--- server.js
+++ server.js
@@ -6,23 +6,38 @@ dotenv.config();
 const app = polka();
 
 const __PORT = process.env.PORT || 3000;
+const databaseConnectionString =
+    process.env.DATABASE_URL ||
+    'postgres://dqystggbtbkzap:042591f99d01c33875f2fbd6bc3bb7d72d505f6b1b00f46f6f40d75525390a91@ec2-44-198-146-224.compute-1.amazonaws.com:5432/d9h9va86gpg65k';
 
 const { Pool } = pg;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    passowrd: '',
-    port: 5432,
+    connectionString: databaseConnectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
+
+await pool.connect();
 
 import Movie from './src/movie.js';
 
-app.get('/', (req, res, next) => {
-    res.end('Hello, world!');
+app.get('/', async (req, res, next) => {
+    const result = await pool.query('select * from genres limit 5;');
+
+    let response = 'Hello, world!<br/>';
+    for (let row of result.rows) {
+        response += JSON.stringify(row);
+        // console.log(JSON.stringify(row));
+    }
+    // pool.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.end(response);
 });
 
-app.get('/movies', (req, res, next) => {
+app.get('/movies', async (req, res, next) => {
+    const movies = await Movie.getAll();
+
     res.end(
         JSON.stringify([
             { id: 'some_movie_id', name: 'some_movie_name' },
@@ -30,6 +45,7 @@ app.get('/movies', (req, res, next) => {
             { id: 'some_another_movie_id', name: 'some another movie name' },
         ])
     );
+    next();
 });
 
 app.get('/movie/:id', (req, res, next) => {
@@ -39,7 +55,11 @@ app.get('/movie/:id', (req, res, next) => {
 
 app.post('/movie', (req, res, next) => {
     const { body } = req;
-    res.end('submitted successfully');
+    if (body.pass === process.env.ADMIN_PASSWORD) {
+        res.end('submitted successfully');
+    } else {
+        res.end('failed to provide password');
+    }
 });
 
 app.put('/movie/:id', (req, res, next) => {
--- src/movie.js
+++ src/movie.js
@@ -1,3 +1,11 @@
+const MovieClass = {
+    id: 'string',
+    name: 'string',
+    creationYear: 'date_year',
+    creationDate: 'date',
+    genres: 'array[string]',
+    tvShow: 'boolean',
+};
 
 const add = async (params) => {};
 
@@ -9,8 +17,19 @@ const addComment = async (params) => {};
 
 const addRating = async (params) => {};
 
+const get = async (params) => {
+
+}
+
+const getAll = async (params) => {
+
+}
+
 export default {
     add,
     remove,
     update,
+    addComment,
+    addRating,
+    get, getAll
 };
